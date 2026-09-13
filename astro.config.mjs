d40fe9187abd069d29848497e640ea80c644b877
Marcos Mendez <marcos.m1207@gmail.com>
2026-09-13 00:13:29 -0300

    seo: unify domain to rominamendezpsicologia.com and add canonical tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rominamenendez.com',
+  site: 'https://rominamendezpsicologia.com',
   output: 'static',
 });
